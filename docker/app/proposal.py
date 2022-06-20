bad7da6427a464009838295e74e4648f75174524
Bogdan Abaev <abaevbog@gmail.com>
2022-06-20 15:06:05 -0400

    Template from aws s3

---
+++ docker/app/proposal.py
@@ -0,0 +1,39 @@
+from jinja2 import Template
+import pdfkit
+import random
+import requests 
+import boto3
+import datetime
+from flask import Flask, request
+BUCKET = "basementremodeling-archive-12345"
+s3 = boto3.client('s3')
+TEMPLATE_URL = "https://basementremodeling-com-estimation-tool.s3.amazonaws.com/static/template.html"
+
+app = Flask(__name__)
+
+
+@app.route("/check",methods=['GET'])
+def health_check():
+    return "CHECK"
+
+@app.route("/",methods=['POST'])
+def make_proposal():
+    body = request.json
+    print(body)
+    r = requests.get(TEMPLATE_URL)
+    jinja_t = Template(r.text)
+    rendered = jinja_t.render(data=body)
+    pdfkit.from_string(rendered, './tmp/proposal.pdf')
+
+    ts = datetime.datetime.now().timestamp()
+    with open("/tmp/proposal.pdf", "rb") as f:
+       s3.upload_fileobj(f, BUCKET, f"temp/proposal_{ts}.pdf",ExtraArgs={'ACL':'public-read'} )
+
+    response = {
+            'statusCode': 200,
+            'body': {"proposal" : f"https://{BUCKET}.s3.amazonaws.com/temp/proposal_{ts}.pdf"}
+        }
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
